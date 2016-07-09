a157c09d125b1d1d8569f45ba33558173412f007
Leoberson Lopes <leobersonlopes@hotmail.com>
2016-07-09 20:33:02 -0300

    Iniciando criação da pagina inicial

--- src/profissionais/views.py
+++ src/profissionais/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from especialidades.models import  Especialidade
 
 # Create your views here.
+
+def index(request):
+
+	return render(request, 'index.html', {"especialidade": Especialidade.objects.all()})
