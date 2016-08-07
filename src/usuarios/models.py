d25c3dde5ab6bf1ebf84672f9e8d785dd6ce4800
dutraneto <dudutraneto@gmail.com>
2016-08-06 22:33:33 -0300

    trying

--- src/usuarios/models.py
+++ src/usuarios/models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
 from pessoas.models import (Pessoa)
@@ -8,7 +9,7 @@ class Usuario(Pessoa):
 
     email = models.CharField(verbose_name='Email', max_length=80)
     senha = models.CharField(verbose_name='Senha', max_length=80)
-    
+
     def __str__(self):
         return self.especialidade
 
